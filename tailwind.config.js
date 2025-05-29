ef2ee28041e2848c085d4e9ebc0cb661acaa6f52
Breno Tardelli <brenot.dev@gmail.com>
2025-05-29 16:02:37 +0200

    adding styles on components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto",
+    },
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
