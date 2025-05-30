43bde6a1f4b2f2251c10d5e96f18016c2fc34f29
Breno Tardelli <brenot.dev@gmail.com>
2025-05-30 16:15:05 +0200

    adding styles on components

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -7,7 +7,7 @@ function MenuItem({ pizza }) {
   return (
     <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className="h-24 rounded-md shadow-md" />
-      <div className="flex flex-grow flex-col">
+      <div className="flex flex-grow flex-col pt-0.5">
         <p>{name}</p>
         <p className="text-sm capitalize italic">{ingredients.join(", ")}</p>
         <div className="mt-auto flex items-center justify-between">
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled, to }) {
+export default function Button({ children, disabled, to, type }) {
   const clasName =
     "inline-block rounded-lg bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 shadow-md transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
+  const base =
+    "inline-block rounded-lg bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 shadow-md transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3",
+    small: base + " p-2 text-xs",
+  };
+
   if (to)
     return (
-      <Link to={to} className={clasName}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   return (
-    <button to={to} disabled={disabled} className={clasName}>
+    <button to={to} disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
